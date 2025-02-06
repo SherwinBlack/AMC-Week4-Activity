3256ba59aaff30f2fd405d4b6307e354e6e9448f
Sherwin Relampagos <sherwinblack21@gmail.com>
2025-02-06 12:59:54 +0800

    ScrollView

---
+++ Activity1/scrollview/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StyleSheet, Text, ScrollView, StatusBar} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container} edges={['top']}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+          I am the way for you to understand the way to live a better life
+          the function of a humana body is a word of art 
+          thats why we mortal needs to sleep
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+  },
+  text: {
+    fontSize: 42,
+    padding: 12,
+  },
+});
+
+export default App;
